884b705994fc4ddbb8d369355521d7ee20ed957e
montanaeli <emont@dolby.com>
2023-11-18 17:13:57 -0300

    Adding owner tests, wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,5 +13,17 @@ module.exports = {
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts"
+  },
+  networks: {
+    sepolia: {
+        chainId:  11155111,
+        timeout:  20000,
+        gasPrice: 8000000000,
+        gas:      "auto",
+        name:     "Sepolia",		
+        url:      process.env.SEPOLIA_ACCESSPOINT_URL,
+        from:     process.env.SEPOLIA_ACCOUNT,
+        accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    }
   }
 };
