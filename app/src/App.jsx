7886fd8e2885c24549cb1cd0359c05ff13c840ea
Nicolás Andreoli Sandler <84604929+nicoandreoli2000@users.noreply.github.com>
2023-11-25 16:30:30 -0300

    Feat/front (#18)
    * feat: vite create react app
    
    * feat: front boilerplate
    
    * feat: added icon
    
    * fix: delete react

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className="flex justify-center bg-blue-300 w-full p-2">
+        <h1 className='text-3xl'>Rubie World</h1>
+      </header>
+      <main>
+        TODO
+      </main>
+      <footer className="p-2">
+        <p>Copyright @ 2023 ORT</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
